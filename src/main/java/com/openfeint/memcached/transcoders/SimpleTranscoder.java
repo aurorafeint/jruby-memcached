1c43b36bed853675aec3ae73dbbf401636c8eb82
Richard Huang <flyerhzm@gmail.com>
2012-07-20 17:59:32 +0800

    add SimpleTranscoder not to do any serialize/deserialize and use pom to organize java code

---
+++ src/main/java/com/openfeint/memcached/transcoders/SimpleTranscoder.java
@@ -0,0 +1,51 @@
+package com.openfeint.memcached.transcoders;
+
+import net.spy.memcached.CachedData;
+import net.spy.memcached.transcoders.Transcoder;
+
+/**
+ *
+ * SimpleTranscoder didn't do any serializing/deserializing or compression/decompression.
+ * Ruby will convert object to string by Marshal and finally passing bytes[].
+ *
+ */
+public class SimpleTranscoder implements Transcoder<Object> {
+    private final int maxSize;
+    private final int flags;
+
+    public SimpleTranscoder() {
+        this(CachedData.MAX_SIZE, 0);
+    }
+
+    public SimpleTranscoder(int flags) {
+        this(CachedData.MAX_SIZE, flags);
+    }
+
+    public SimpleTranscoder(int maxSize, int flags) {
+        this.maxSize = maxSize;
+        this.flags = flags;
+    }
+
+    public boolean asyncDecode(CachedData d) {
+        return false;
+    }
+
+    public CachedData encode(Object o) {
+        byte[] b = (byte[]) o;
+        return new CachedData(getFlags(), b, getMaxSize());
+    }
+
+    public Object decode(CachedData d) {
+        byte[] data = d.getData();
+        int flags = d.getFlags();
+        return data;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public int getFlags() {
+        return flags;
+    }
+}
