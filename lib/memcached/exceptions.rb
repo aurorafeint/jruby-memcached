999be8c339ed4d6155fa2a22150216713f881a57
Richard Huang <flyerhzm@gmail.com>
2012-08-15 08:31:37 +0800

    remove unused file lib/memcached/exceptions.rb

--- lib/memcached/exceptions.rb
+++
@@ -1,4 +0,0 @@
-class Memcached::Error < RuntimeError; end
-class Memcached::NotFound < Memcached::Error; end
-class Memcached::NotStored < Memcached::Error; end
-class Memcached::NotSupport < Memcached::Error; end
