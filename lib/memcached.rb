ba9440cc7970fcdf1de47d136c77e84bf0a22543
Richard Huang <flyerhzm@gmail.com>
2012-08-16 22:49:08 +0800

    we can require MemcachedService directly

--- lib/memcached.rb
+++ lib/memcached.rb
@@ -1,5 +1,4 @@
 require 'java'
 require 'memcached/version'
 require File.join(File.dirname(__FILE__), '../target/spymemcached-ext-0.0.1.jar')
-
-com.openfeint.memcached.MemcachedService.new.basicLoad(JRuby.runtime)
+require 'com/openfeint/memcached/memcached'
